fef384e046d10b3dbc8dc3f226598a66cf99526a
Muhammad Hasan <hasan@sirus.dev>
2019-10-21 10:08:55 +0700

    menambahkan dockerfile dengan fungsi print

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:12-alpine
+
+WORKDIR /app
+COPY . .
+
+ENTRYPOINT [ "node", "." ]
+CMD [ "guys" ]
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const { printFolder } = require("./file");
+const { printHai } = require("./print");
 
-printFolder();
+printHai();
