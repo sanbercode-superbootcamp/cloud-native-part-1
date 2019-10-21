5dda51e5e6dc1c74edafc4ac4535377b21c8fd30
Muhammad Hasan <hasan@sirus.dev>
2019-10-21 11:43:44 +0700

    menambahkan file service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { serveHai } = require("./server");
 const { printHai } = require("./print");
+const { printFolder } = require("./file");
 
 const arg = process.argv[2];
 switch (arg) {
@@ -8,6 +9,9 @@ switch (arg) {
     break;
   case "server":
     serveHai();
+    break;
+  case "file":
+    printFolder();
     break;
   default:
     console.log("node . server | print");
